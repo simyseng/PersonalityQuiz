e09c8eb6717cdc079d3b56820bedc196f5dfca65
Sim Yong Seng <simyseng@gmail.com>
2019-04-13 22:03:35 +0700

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Sim Yong Seng on 13/4/19.
+//  Copyright © 2019 Sim Yong Seng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
