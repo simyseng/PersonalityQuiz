92d35ed37c0678eb12d1a55b31bfe0f8b3042e55
Sim Yong Seng <simyseng@gmail.com>
2019-04-14 16:39:28 +0700

    Display questions with the right control

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var rangedStackView: UIStackView!
+    
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Steak", type: .dog),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrots", type: .rabbit),
+                    Answer(text: "Corn", type: .turtle)
+                ]),
+        Question(text: "Which activities do you enjoy ?",
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .dog)
+                ]),
+        Question(text: "How do you enjoy car rides?",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get a little nervous", type: .rabbit),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I love them", type: .dog)
+                ])
+        ]
+    
+    var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        navigationItem.title = "Question #\(questionIndex + 1)"
+        
+        let currentQuestion = questions[questionIndex]
+        
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
     }
-    */
 
 }
